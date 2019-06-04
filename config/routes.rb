26830e94b728113982a4455b37f769e8a7d9ea7b
Mathieu Bordeleau <bordeleau.mathieu@gmail.com>
2019-06-04 17:32:15 +0200

    bar rating ok for appliances

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  resources :uses, only: [:new, :create]
   resources :appliances, only: [:index, :show, :new, :create, :edit, :update, :destroy]
   resources :projects, only: [:index, :show, :new, :create, :edit, :update, :destroy]
 end
