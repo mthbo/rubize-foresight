02ee947baea8fd77a688bfc6d71fa7c2a45e0531
Mathieu Bordeleau <bordeleau.mathieu@gmail.com>
2019-08-05 17:40:30 +0200

    debug ajax callback

--- app/javascript/plugins/loader.js
+++ app/javascript/plugins/loader.js
@@ -2,29 +2,19 @@ const toggleLoader = () => {
   let forceClose = false;
   const $customLoader = $("#custom-loader");
   $customLoader.on('shown.bs.modal', function() {
-    console.log("shown.bs.modal");
-    console.log(forceClose);
     if (forceClose) {
       $customLoader.modal('hide');
-      console.log("force closing");
-      console.log(forceClose);
     }
   });
   $customLoader.on('hidden.bs.modal', function() {
     forceClose = false;
-    console.log("hidden.bs.modal");
-    console.log(forceClose);
   });
   document.body.addEventListener('ajax:send', function(event) {
     $customLoader.modal('show');
-    console.log("ajax:send");
-    console.log(forceClose);
   });
   document.body.addEventListener('ajax:complete', function(event) {
     forceClose = true;
     $customLoader.modal('hide');
-    console.log("ajax:complete");
-    console.log(forceClose);
   });
 };
 
