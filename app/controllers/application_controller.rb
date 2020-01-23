bc87cae82b1e511de11458be6e2d97339a385a46
Mathieu Bordeleau <bordeleau.mathieu@gmail.com>
2020-01-23 13:34:28 +0100

    design improvement for users, navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,13 +6,7 @@ class ApplicationController < ActionController::Base
   after_action :verify_authorized, except: :index, unless: :skip_pundit?
   after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
 
-  before_action :configure_permitted_parameters, if: :devise_controller?
-
   private
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
-  end
 
   def skip_pundit?
     devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -13,6 +13,7 @@ class RegistrationsController < Devise::RegistrationsController
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end
 
   def redirect_unless_admin
